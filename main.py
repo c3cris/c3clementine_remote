ca4aa6a6af572bbc9ca8a50c0a4559e73e20b363
cris <c3cris@gmail.com>
2018-09-22 17:07:35 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,268 @@
+import select, traceback
+import inspect, sys, math
+from PIL import Image
+from pprint import pprint
+import clementine_pb2 as cr
+import pygame as pg
+from player import Player
+from clemwrapper import ClemWrapper
+
+CAPTION = "c3clementine"
+SCREEN_SIZE = (int(1260), int(720))
+WIDTH = SCREEN_SIZE[0]
+HEIGHT = SCREEN_SIZE[1]
+
+
+
+class Control(object):
+
+  def __init__(self):
+
+    self.screen = pg.display.get_surface()
+    self.clock = pg.time.Clock()
+    self.fps = 30
+    self.done = False
+    self.viewport = self.screen.get_rect()
+    self.room = pg.Surface((4000,4000)).convert_alpha()
+    self.room_rect = self.room.get_rect()
+    self.art = pg.Surface((400,400)).convert_alpha()
+
+    self.playlists = pg.Surface((400,400)).convert_alpha()
+    self.playlists_rect = self.playlists.get_rect(topleft=(50, 400))
+
+    self.songs = pg.Surface((600,600)).convert_alpha()
+    self.songs_rect = self.songs.get_rect(topleft=(550, 150))
+
+    self.pg = pg
+    self.p = Player(self.pg, self)
+    self.keys = self.pg.key.get_pressed()
+
+    self.songs.fill(color=(255,255,255,0))
+    self.playlists.fill(color=(255,255,255,0))
+
+    self.connection = ClemWrapper(self.p,"192.168.1.165", 5500, 1234,
+                            False, True, 21 )
+
+  def display_fps(self):
+    caption = "{} - fps: {:.2f}".format(CAPTION, self.clock.get_fps())
+    self.pg.display.set_caption(caption)
+
+
+
+
+  def draw(self):
+
+    self.room.fill(color=(255,255,255,0))
+    self.screen.fill(color=(255,255,255))
+
+    if self.p.update_art and self.p.art is not None:
+      self.art.fill(color=(255,255,255,0))
+      self.art.blit(self.p.art, (0, 0))
+      self.update_art = False
+
+
+    self.screen.blit(self.pg.transform.smoothscale(self.art, (200, 200)), (290,100))
+
+
+    
+    if self.p.songs_change: 
+      self.songs.fill(color=(255,255,255,0))
+    
+    if self.p.playlist_change: 
+      self.playlists.fill(color=(255,255,255,0))
+
+
+    self.p.draw(self.room)
+
+    self.screen.blit(self.songs, self.songs_rect)
+    self.screen.blit(self.playlists, self.playlists_rect)
+    self.screen.blit(self.room,(0, 0), self.viewport)
+
+
+
+
+  def draw_text(self, msg, x ,y, size=20, color = "blue"):
+    font_type = pg.font.match_font("ubuntu")
+    font = pg.font.Font(font_type, size)
+    text = font.render(unicode(msg), True, self.pg.Color(color))
+    rect = text.get_rect(topleft=(x, y))
+    return text, rect
+
+  def event_loop(self):
+    for event in pg.event.get():
+      if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
+        self.done = True
+        self.connection.connected = False
+        self.connection.thread.join()
+        self.connection.soc.close()
+      elif event.type == pg.KEYDOWN:
+        if event.key == pg.K_LEFT:
+          msg = cr.Message()
+          msg.type = cr.SET_VOLUME
+          msg.request_set_volume.volume = int(self.p.volume - 10)
+          self.send_message(msg)
+
+        elif event.key == pg.K_RIGHT:
+          msg = cr.Message()
+          msg.type = cr.SET_VOLUME
+          msg.request_set_volume.volume = int(self.p.volume + 10)
+          self.send_message(msg)
+
+      elif event.type == pg.MOUSEBUTTONDOWN:
+        if event.button == 1:
+          x, y = self.pg.mouse.get_pos()
+          if self.p.volume_rect.collidepoint((x,y)):
+            self.p.updating_volume = True
+          if self.p.position_rect.collidepoint((x,y)):
+            self.p.updating_position = True
+
+      elif event.type == pg.MOUSEBUTTONUP:
+        x, y = self.pg.mouse.get_pos()
+
+        if event.button == 1:
+          if self.p.updating_volume:
+            self.p.updating_volume = False
+            msg = cr.Message()
+            msg.type = cr.SET_VOLUME
+            msg.request_set_volume.volume = self.p.volume
+            self.connection.send_message(msg)
+
+          if self.p.updating_position:
+            self.p.updating_position = False
+            msg = cr.Message()
+            msg.type = cr.SET_TRACK_POSITION
+            msg.request_set_track_position.position = self.p.position
+            self.connection.send_message(msg)
+
+          if self.p.prev.collidepoint((x,y)):
+            msg = cr.Message()
+            msg.type = cr.PREVIOUS
+            self.connection.send_message(msg)
+          elif self.p.next.collidepoint((x,y)):
+            msg = cr.Message()
+            msg.type = cr.NEXT
+            self.connection.send_message(msg) 
+          elif self.p.pause_play.collidepoint((x,y)):
+            msg = cr.Message()
+            msg.type = cr.PLAYPAUSE
+            self.connection.send_message(msg) 
+
+          if self.songs_rect.collidepoint((x,y)):
+            if self.p.active_playlist:
+              msg = cr.Message()
+              msg.type = cr.CHANGE_SONG
+
+              song_o_index = int( math.floor(min(y - 10 - self.songs_rect.y, 25 * 22) / 22) 
+                + self.p.song_start)
+
+              song = self.p.songs[self.p.active_playlist][song_o_index]
+
+              # pprint((y - 10, 25 * 22, min(y - 10, 25 * 22) / 22 ))
+
+              msg.request_change_song.playlist_id = self.p.active_playlist
+              msg.request_change_song.song_index = song["index"]
+              self.connection.send_message(msg)
+              self.songs_change = True
+ 
+          if self.playlists_rect.collidepoint((x,y)):
+
+            msg = cr.Message()
+            msg.type = cr.REQUEST_PLAYLIST_SONGS
+
+            playlist_index = int(
+              min(len(self.p.playlists),
+               max(0, math.floor(y - 10 - self.playlists_rect.y - self.p.playlists_rect.y) / 22)))
+           
+            playlist_id = self.p.playlists_order[playlist_index]
+
+            msg.request_playlist_songs.id = playlist_id
+            
+            self.connection.send_message(msg)
+            self.playlists_change = True
+ 
+        if self.playlists_rect.collidepoint((x,y)):
+          if event.button == 4:
+
+            self.p.playlists_rect.move_ip(0, 20)
+            self.p.playlist_change = True
+          elif event.button == 5:
+            #down
+            self.p.playlists_rect.move_ip(0, -20)
+            self.p.playlist_change = True 
+
+        if self.songs_rect.collidepoint((x,y)):
+          if event.button == 4:
+
+            self.p.song_start -= 5
+            self.p.songs_change = True
+          elif event.button == 5:
+            #down
+            self.p.song_start += 5
+            self.p.songs_change = True
+
+      if event.type == pg.USEREVENT:
+          self.pg.time.set_timer(self.pg.USEREVENT, 0)
+
+
+
+
+  def update(self):
+    self.keys = pg.key.get_pressed()
+    self.p.update()
+
+  def main_loop(self):
+    self.pg.time.set_timer(self.pg.USEREVENT, 2000)
+
+    while not self.done:
+      try:
+        self.event_loop()
+        self.update()
+        self.draw()
+        pg.display.update()
+        if self.p.playlist_change_b == self.p.playlist_change:
+          self.p.playlist_change = False
+        if self.p.songs_change_b == self.p.songs_change:
+          self.p.songs_change = False
+        if self.p.active_playlist == self.p.new_active_pl:
+          self.p.new_active_pl = False
+
+
+        self.clock.tick(self.fps)
+        self.display_fps()
+      except Exception as e:
+        pprint(e)
+        print(traceback.format_exc())
+        print("error occured closing gracefully")
+        self.done = True
+        self.connection.connected = False
+        self.connection.thread.join()
+        self.connection.soc.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+  pg.init()
+  pg.display.set_caption(CAPTION)
+  pg.display.set_mode(SCREEN_SIZE)
+  run = Control()
+  run.main_loop()
+  pg.quit()
+  sys.exit()
---
+++ player.py
@@ -0,0 +1,186 @@
+import math 
+from io import BytesIO
+from pprint import pprint
+
+
+class Player(object):
+
+  def __init__(self, pg, control):
+    self.position = 0
+    self.volume = 0
+    self.track = None
+    self.songs = {}
+    self.playlists = None
+    self.playlists_order = None
+    self.shuffle = 0
+    self.song_start = 0
+    self.clem_update = False
+    self.state = ""
+    self.pg = pg
+    self.control = control
+    self.updating_volume = False
+    self.updating_position = False
+    self.x_offset = 50
+    self.volume_rect = pg.Rect((self.x_offset, 250),(8,8))
+    self.volume_rect.center=(self.x_offset, 250)
+    self.position_rect = pg.Rect((self.x_offset, 250),(8,8))
+    self.position_rect.center=(self.x_offset, 300)
+    self.prev = pg.Rect((self.x_offset, 320),(60,25))
+    self.next = pg.Rect((self.x_offset + 150, 320),(60,25))
+    self.pause_play = pg.Rect((self.x_offset + 70, 320),(60,25))
+    self.play_status = "PLAY"
+    self.playlists_rect = pg.Rect((0, 0),(400,400))
+    self.songs_rect = pg.Rect((0, 0),(500,600))
+    self.active_playlist = 0
+    self.update_art = False
+    self.art = None
+    self.new_active_pl = False
+    self.playlist_change = False
+    self.songs_change = False
+    self.playlist_change_b = self.playlist_change
+    self.songs_change_b = self.songs_change
+
+  def update(self):
+
+    if self.new_active_pl:
+      self.active_playlist = self.new_active_pl
+      self.songs_change = True
+      self.playlist_change = True
+
+    self.playlist_change_b = self.playlist_change
+    self.songs_change_b = self.songs_change
+
+    if self.updating_volume:
+      x, y = self.pg.mouse.get_pos()
+      new_x = max(self.x_offset, min(self.x_offset+200,x))
+      if new_x != self.volume_rect.x:
+        new_volume = int(((float(new_x) - self.x_offset ) / 200) * 100)
+        self.control.connection.set_volume(new_volume)
+
+        self.volume = new_volume
+        self.volume_rect.x = int(new_x)
+
+    if self.updating_position:
+      x, y = self.pg.mouse.get_pos()
+      new_x = max(self.x_offset, min(self.x_offset+200,x))
+      if new_x != self.position_rect.x:
+        new_position = int(((float(new_x) - self.x_offset ) / 200) * self.track.length)
+        self.position = new_position
+        self.position_rect.x = int(new_x)
+
+  def draw(self, surf):
+    y = 30
+    if self.track is not None:
+
+      surf.blit(*self.control.draw_text("Track: " + unicode(self.track.title), self.x_offset, y))
+      y+=20
+      surf.blit(*self.control.draw_text("Artist: " + unicode(self.track.artist), self.x_offset, y))
+      y+=20
+      surf.blit(*self.control.draw_text("Album: " + unicode(self.track.album), self.x_offset, y))
+
+      surf.blit(*self.control.draw_text("Volume: " + unicode(self.volume), self.x_offset, 220))
+
+
+      surf.blit(*self.control.draw_text("Position: " + 
+        self.get_formatted_position(self.position) + " / " +
+        self.track.pretty_length, self.x_offset, 270))
+      surf.blit(*self.control.draw_text("State: " + unicode(self.state), self.x_offset, 140))
+    
+      surf.blit(*self.control.draw_text("PREV", self.prev.x,  self.prev.y, 25))
+      surf.blit(*self.control.draw_text(self.play_status, self.pause_play.x,  self.prev.y, 25))
+      surf.blit(*self.control.draw_text("NEXT", self.next.x,  self.next.y, 25))
+
+      # self.pg.draw.rect(surf, pg.Color("red"), self.next, 3)
+      # self.pg.draw.rect(surf, pg.Color("red"), self.prev, 3)
+      # self.pg.draw.rect(surf, pg.Color("red"), self.pause_play, 3)
+
+      surf.blit(*self.control.draw_text("Playlists: ", 50, 370))
+      surf.blit(*self.control.draw_text("Songs: ", 550, 120))
+
+    if self.playlists is not None and self.playlist_change:
+      self.draw_playlists(self.control.playlists)
+
+
+    if self.active_playlist in self.songs and self.songs_change:
+      self.draw_songs(self.control.songs)
+      
+
+    self.draw_position(surf)
+    self.draw_volume(surf)
+    self.draw_art()
+
+  def draw_position(self, surf):
+
+    if self.track is not None and self.track.index != 0:
+
+      self.pg.draw.line(surf, self.pg.Color("green"), (self.x_offset,300), (self.x_offset + 200,300),3)
+      pos = float(self.position) / float(self.track.length)
+      self.position_rect.x = int(pos * 200) + self.x_offset
+      self.pg.draw.circle(surf, self.pg.Color("black"), self.position_rect.center, 5)
+
+  def draw_volume(self, surf):
+
+    self.pg.draw.line(surf, self.pg.Color("green"), (self.x_offset,250), (self.x_offset+200,250),3)
+    pos = float(self.volume) / 100
+    self.volume_rect.x = pos*200 + self.x_offset
+    self.pg.draw.circle(surf, self.pg.Color("black"), self.volume_rect.center, 5)
+
+  def draw_playlists(self, surf):
+
+    y = 10 + self.playlists_rect.y
+
+    for playlist in self.playlists:
+
+      if self.playlists[playlist]["closed"]:
+        color = "lightgray"
+      else:
+        color = "black"
+      if self.playlists[playlist]["active"]:
+        color = "green"
+
+      surf.blit(*self.control.draw_text(unicode(self.playlists[playlist]["name"]) + 
+        "  [" + unicode(self.playlists[playlist]["item_count"]) + "]", 15, y, 20, color))
+      y += 22
+
+
+
+  def draw_songs(self, surf):
+    y = 10
+    cur_playlist_id = self.active_playlist
+
+    if self.song_start < 0:
+      self.song_start = 0
+    elif self.song_start + 25 > self.playlists[cur_playlist_id]["item_count"]:
+      self.song_start = self.playlists[cur_playlist_id]["item_count"] - 25
+
+    for i in xrange(self.song_start, self.song_start + 25):
+
+      color = "black"
+
+      if i < len(self.songs[cur_playlist_id]) and self.songs[cur_playlist_id][i]["id"] == self.track.id:
+        color = "green"
+
+      surf.blit(*self.control.draw_text(
+        unicode(self.songs[cur_playlist_id][i]["title"][0:30]) + 
+        "  [" + unicode(self.songs[cur_playlist_id][i]["artist"]) + "]", 15, y, 20, color))
+      y += 22
+
+
+    self.pg.draw.line(surf, self.pg.Color("gray"), (595,0), (595,550),3)
+    pos = float(self.song_start)  / float(self.playlists[cur_playlist_id]["item_count"] - 25)
+    new_y = int( pos * 550 + 5 )
+    self.pg.draw.circle(surf, self.pg.Color("black"), (595, new_y), 5)
+
+  def get_formatted_position(self, pos):
+    pos = float(pos)
+    hour = minute = second = 0
+    hour = int(math.floor(pos / 3600))
+    minute = int((pos / 60)) % 60
+    second = int(pos % 60)
+    return "{h}:{m}:{s}".format(s=second, m=minute, h=hour)
+
+  def draw_art(self):
+    if self.update_art and self.track.art != "":
+      img = BytesIO(self.track.art)
+      self.art = self.pg.image.load(img)
+
